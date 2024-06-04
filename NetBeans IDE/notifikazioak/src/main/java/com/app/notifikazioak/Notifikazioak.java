ee2d60203e7eba03d0e19c17b2f670c89ba6635b
aKijn2 <mrkinder20041010@gmail.com>
2024-06-04 09:18:03 +0200

    using netbeans

---
+++ NetBeans IDE/notifikazioak/src/main/java/com/app/notifikazioak/Notifikazioak.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.app.notifikazioak;
+
+/**
+ *
+ * @author 1ag3.achralla
+ */
+public class Notifikazioak {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
